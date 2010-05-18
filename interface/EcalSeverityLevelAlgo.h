5dccc075900a7b1cab4205daf609d52820bb2b8b
cmsbuild <>
2010-05-18 15:47:51 +0000

    This commit was manufactured by cvs2git to create tag 'RecoLocalCalo- EcalRecAlgos-V00-11-02-01'.
    Sprout from master 2010-02-08 23:26:18 UTC Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch> 'correct typo'
    Cherrypick from master 2010-05-18 15:47:50 UTC Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch> 'add method to detect spikes from the kOutOfTime flag (from Frank) for recHit > energy threshold':
        interface/EcalSeverityLevelAlgo.h
        src/ESRecHitSimAlgo.cc
        src/EcalSeverityLevelAlgo.cc

--- interface/EcalSeverityLevelAlgo.h
+++ interface/EcalSeverityLevelAlgo.h
@@ -11,66 +11,61 @@ class EcalSeverityLevelAlgo {
                 // Levels of severity:
                 // - 0 --> good
                 // - 1 --> problematic (e.g. noisy)
-                // - 2 --> recovered (e.g. dead or saturated)
-                // - 3 --> bad, not suitable to be used in the reconstruction
+                // - 2 --> recovered (e.g. an originally dead or saturated)
+                // - 3 --> weird (e.g. spike)
+                // - 4 --> bad, not suitable to be used in the reconstruction
 
-                enum EcalSeverityLevel { kGood, kProblematic, kRecovered, kBad, kWeird };
-                
-                static int severityLevel( const DetId , const EcalRecHitCollection &, const EcalChannelStatus & );
+                enum EcalSeverityLevel { kGood=0, kProblematic, kRecovered, kWeird, kBad };
+
+                enum SpikeId { kE1OverE9=0, kSwissCross, kSwissCrossBordersIncluded };
+
+                /** compute the severity level
+                 */
+                static int severityLevel( const DetId,
+                                          const EcalRecHitCollection &,
+                                          const EcalChannelStatus &,
+                                          float recHitEtThreshold = 5.,
+                                          SpikeId spId = kSwissCross,
+                                          float spIdThreshold = 0.95,
+					  bool useTiming = false,
+					  float recHitEnergyThreshold = -999.
+                                          );
+
+                /** return the estimator of the signal being a spike
+                 *  based on the topological information from the neighbours
+                 */
+                static float spikeFromNeighbours( const DetId id,
+                                                  const EcalRecHitCollection &,
+                                                  float recHitEtThreshold,
+                                                  SpikeId spId
+                                                  );
+
+                /** ratio between the crystal energy and the energy in the 3x3
+                 *  matrix of crystal
+                 */
+                static float E1OverE9( const DetId id, const EcalRecHitCollection &, float recHitEtThreshold = 0. );
+
+                /** 1 - the ratio between the energy in the swiss cross around
+                 * a crystal and the crystal energy (also called S4/S1, Rook)
+                 */
+                static float swissCross( const DetId id, const EcalRecHitCollection &, float recHitEtThreshold = 0. , bool avoidIeta85=true);
+
+		/** return whether or not the rechit is a spike based on the kOutOfTime rechit flag
+                 */
+		static bool spikeFromTiming( const EcalRecHit &, float recHitEnergyThreshold );
+
+        private:
+
                 static int severityLevel( uint32_t rhFlag, uint16_t dbStatus );
                 static int severityLevel( const EcalRecHit &, const EcalChannelStatus & );
 
-        private:
                 static uint16_t retrieveDBStatus( const DetId , const EcalChannelStatus &chStatus );
 
-                /** return the estimator of the signal being a spike
-                 *  based on the neighbours energies
-                 */
-                static float spikeFromNeighbours( const DetId id , const EcalRecHitCollection & );
                 /** return energy of a recHit (if in the collection)
                  */
-                static float recHitEnergy( const DetId id, const EcalRecHitCollection &recHits );
-
-                // HOME MADE NAVIGATION - to be generalized
-                // (and so moved in a more appropriate place)
-
-                /** Converts a std CMSSW crystal eta index to a c-array index (starting from
-                 * zero and without hole).
-                 */
-                static int iEta2cIndex(int iEta){
-                        return (iEta<0)?iEta+85:iEta+84;
-                }
-                /** Converts a std CMSSW crystal phi index to a c-array index (starting from
-                 * zero and without hole).
-                 */
-                static int iPhi2cIndex(int iPhi){
-                        int iPhi0 = iPhi - 11;
-                        if(iPhi0<0) iPhi0 += 360;
-                        return iPhi0;
-                }
-                /** converse of iEta2cIndex() method.
-                */
-                static int cIndex2iEta(int i){
-                        return (i<85)?i-85:i-84;
-                }
-                /** converse of iPhi2cIndex() method.
-                */
-                static int cIndex2iPhi(int i){
-                        return 1+ ((i+10) % 360);
-                }
-                /** Converts a std CMSSW crystal x or y index to a c-array index (starting
-                 * from zero and without hole).
-                 */
-                int iXY2cIndex(int iX) const{
-                        return iX-1;
-                }
-                /** converse of iXY2cIndex() method.
-                */
-                int cIndex2iXY(int iX0) const{
-                        return iX0+1;
-                }
-
-
+                static float recHitE( const DetId id, const EcalRecHitCollection &recHits );
+                static float recHitApproxEt( const DetId id, const EcalRecHitCollection &recHits );
+                static float recHitE( const DetId id, const EcalRecHitCollection & recHits, int dEta, int dPhi );
 };
 
 #endif
