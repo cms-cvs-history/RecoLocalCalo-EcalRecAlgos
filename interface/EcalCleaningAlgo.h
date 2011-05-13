a4f3f56353582be0055ca7dab66a78b5f4773901
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2011-05-13 16:23:12 +0000

    use old-style flagging of kWeird with swisscross rather than PF cleaning

--- interface/EcalCleaningAlgo.h
+++ interface/EcalCleaningAlgo.h
@@ -3,7 +3,7 @@
    Declaration of class EcalCleaningAlgo
 
    \author Stefano Argiro
-   \version $Id: EcalCleaningAlgo.h,v 1.3 2011/03/02 14:37:50 argiro Exp $
+   \version $Id: EcalCleaningAlgo.h,v 1.4 2011/05/12 19:00:34 vlimant Exp $
    \date 20 Dec 2010
 */
 
@@ -35,49 +35,29 @@ public:
 
   void setFlags(EcalRecHitCollection& rhs);
   
+  
+
 private:
   
-  /// yet another function to calculate swiss cross
-  float e4e1(const DetId& id, const EcalRecHitCollection& rhs);
-
-  /**  Compute e6 over e2 around xtal 1, where 2 is the most energetic in 
-       the swiss cross around 1
   
-             | | |   
-           +-+-+-+-+
-           | |1|2| |
-           +-+-+-+-+
-             | | |               */
-  float e6e2 (const DetId& id, const EcalRecHitCollection& rhs);
+  float swissCross( const DetId id, const EcalRecHitCollection & recHits, 
+		    float recHitThreshold , bool avoidIeta85);
 
-  float recHitE( const DetId id, const EcalRecHitCollection &recHits );
+  float recHitE( const DetId id, 
+		 const EcalRecHitCollection & recHits,
+		 int di, int dj );
+
+  float recHitE( const DetId id, 
+		 const EcalRecHitCollection &recHits );
+
   
-  /// in EB, check if we are near a crack
-  bool isNearCrack(const DetId& detid);
+  float recHitApproxEt( const DetId id, 
+			const EcalRecHitCollection &recHits );
 
-  /// return the id of the  4 neighbours in the swiss cross
-  const std::vector<DetId> neighbours(const DetId& id);
-
-  ///ignore kOutOfTime above threshold when calculating e4e1
-  float ignoreOutOfTimeThresh_;
-
-  // Parameters for tolopogical cut 
-  // mark anomalous if e> cThreshold &&  e4e1> a*log10(e1e1)+b
-  float cThreshold_barrel_;
-  float cThreshold_endcap_;         
-  float e4e1_a_barrel_;
-  float e4e1_b_barrel_;
-  float e4e1_a_endcap_;
-  float e4e1_b_endcap_;
-  // when calculating e4/e1, ignore hits below this threshold
-  float e4e1Treshold_barrel_;
-  float e4e1Treshold_endcap_;
-  float tightenCrack_e1_single_;
-  float tightenCrack_e4e1_single_;
-  float cThreshold_double_;
-  float tightenCrack_e1_double_;
-  float tightenCrack_e6e2_double_;
-  float e6e2thresh_;
+  double swissCrossThreshold_;
+  double recHitThreshold_;
+  bool   useieta85_;
+  
 
 };
 
